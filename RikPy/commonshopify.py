98a2649fa868f90b8ce4b1fd16eddc40fe59cc55
Richard Forn <Richard Forn>
2025-10-06 19:16:41 +0200

    fix: deprecated shopify fields 5

--- RikPy/commonshopify.py
+++ RikPy/commonshopify.py
@@ -497,13 +497,9 @@ def Shopify_get_products_query(shop="", access_token="", api_version="2025-01"):
                                     inventoryQuantity
                                     inventoryItem {
                                         id
+                                        weight
+                                        weightUnit
                                         requiresShipping
-                                        measurement {
-                                            weight {
-                                                value
-                                                unit
-                                            }
-                                        }
                                     }
                                 }
                             }
@@ -666,13 +662,9 @@ def Shopify_get_product_variants_mutation(shop="", access_token="", api_version=
               inventoryQuantity
               inventoryItem {
                 id
+                weight
+                weightUnit
                 requiresShipping
-                measurement {
-                  weight {
-                    value
-                    unit
-                  }
-                }
               }
             }
           }
